f37a0a06f1fc106c3e3996db26577545d48cdde0
Austin Goetz <a.goetz09@gmail.com>
2020-10-21 16:49:08 -0600

    add controller with create func

---
+++ PairRandomizer/PairRandomizer/Controllers/ModelControllers/PersonController.swift
@@ -0,0 +1,23 @@
+//
+//  PersonController.swift
+//  PairRandomizer
+//
+//  Created by Austin Goetz on 10/21/20.
+//
+
+import Foundation
+
+class PersonController {
+    
+    // MARK: - Properties
+    /// Shared Instance
+    static let shared = PersonController()
+    /// Source of Truth
+    var people: [Person] = []
+    
+    // MARK: - CRUD
+    func addPerson(name: String) {
+        let newPerson = Person(name: name)
+        people.append(newPerson)
+    }
+}
